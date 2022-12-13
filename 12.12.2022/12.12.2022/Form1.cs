556646c8537ae3f7185d05d6b07590a0da2ef531
zeynep koç <114741443+zeybernep@users.noreply.github.com>
2022-12-13 23:02:53 +0300

    Add files via upload

---
+++ 12.12.2022/12.12.2022/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _12._12._2022
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Random rastgele = new Random();
+            int sayi = rastgele.Next(0,100);
+            progressBar1.Maximum = sayi;
+            MessageBox.Show(sayi.ToString());
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int eklenen = Convert.ToInt32(textBox1.Text);
+            
+            if(progressBar1.Value+eklenen >= progressBar1.Maximum) MessageBox.Show("oyun bitti");
+            else
+            {
+                progressBar1.Value += Convert.ToInt32(textBox1.Text);
+            }
+
+        }
+    }
+}
